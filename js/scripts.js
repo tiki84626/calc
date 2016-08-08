f1634d14f5a262e01995dd778fb74ac855c096f7
Andrew Malkin and Joanna Anderson <andrew@>
2016-08-08 11:25:44 -0700

    add the rest of the operations - subtract, multiply, divide

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,19 @@ var add = function(number1, number2) {
 	return number1 + number2;
 };
 
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiple = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
+var result = divide(number1, number2);
 alert(result);
