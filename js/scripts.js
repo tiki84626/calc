4646869485407d68be89ddd922ee7662f8a19681
Andrew Malkin and Marta Sanchez <marta@>
2016-08-10 10:43:00 -0700

    add user interface for subtract, divide, and multiply

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,48 @@ var add = function(number1, number2) {
 };
 
 var subtract = function(number1, number2) {
-  return number1 - number2;
+	return number1 - number2;
 };
 
-var multiple = function(number1, number2) {
-  return number1 * number2;
+var multiply = function(number1, number2) {
+	return number1 * number2;
 };
 
 var divide = function(number1, number2) {
-  return number1 / number2;
+	return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = divide(number1, number2);
-alert(result);
+
+$(document).ready(function() {
+	$("form#calc").submit(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#number1").val());
+		var number2 = parseInt($("#number2").val());
+		var result = add(number1, number2);
+		$("#output").text(result);
+	});
+
+	$("form#sub").submit(function(event) {
+		event.preventDefault();
+		var number1Sub = parseInt($("#number1-sub").val());
+		var number2Sub = parseInt($("#number2-sub").val());
+		var result = subtract(number1Sub, number2Sub);
+		$("#output").text(result);
+	});
+
+	$("form#div").submit(function(event) {
+		event.preventDefault();
+		var number1Div = parseInt($("#number1-div").val());
+		var number2Div = parseInt($("#number2-div").val());
+		var result = divide(number1Div, number2Div);
+		$("#output").text(result);
+  });
+
+	$("form#mult").submit(function(event) {
+		event.preventDefault();
+		var number1Mult = parseInt($("#number1-mult").val());
+		var number2Mult = parseInt($("#number2-mult").val());
+		var result = multiply(number1Mult, number2Mult);
+		$("#output").text(result);
+  });
+});
